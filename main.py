f378a451a34db294f2b3f46dee68bc865dd9920d
Ahmed Hemida <ahmedhemida121@gmail.com>
2023-09-27 13:05:22 +0300

    #

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
-from sqlalchemy import ForeignKey, Integer, String, Text
+from sqlalchemy import ForeignKey, Integer, Text
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
