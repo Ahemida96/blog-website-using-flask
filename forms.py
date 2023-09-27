dd72ce15eb9be9fbb5868a8820366ae7520a3060
Ahmed Hemida <96353045+Ahemida96@users.noreply.github.com>
2023-09-27 12:20:47 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, EmailField
-from wtforms.validators import DataRequired, URL, Length, Email
+from wtforms.validators import DataRequired, URL, Length
 from flask_ckeditor import CKEditorField
 
 pass_message = "Password must be at least 8 characters long."
@@ -16,7 +16,7 @@ class CreatePostForm(FlaskForm):
 
 
 class RegisterForm(FlaskForm):
-    email = EmailField('Email', validators=[DataRequired(), Email()])
+    email = EmailField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, message=pass_message)])
     name = StringField('Name', validators=[DataRequired()])
     submit = SubmitField('Sign me up!'.upper())
