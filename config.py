a3a770947e377ad2423f26ec1245f2ce8c9a54d5
Ahmed Hemida <ahmedhemida121@gmail.com>
2025-03-17 07:43:14 +0200

    Docker compose and helper files

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+user = os.environ.get('POSTGRES_USER')
+password = os.environ.get('POSTGRES_PASSWORD')
+host = os.environ.get('POSTGRES_HOST')
+port = os.environ.get('POSTGRES_PORT')
+database = os.environ.get('POSTGRES_DB')
